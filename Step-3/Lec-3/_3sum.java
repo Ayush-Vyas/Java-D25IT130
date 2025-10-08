18048f6630f22ef7824f8ff19673c38fcbb45176
Ayush-Vyas <ayushvyas172@gmail.com>
2025-10-08 23:56:25 +0530

    Add files via upload

---
+++ Step-3/Lec-3/_3sum.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class _3sum {  // Class name cannot start with a number
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sort the array
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {-1,0,1,2,-1,-4};
+        System.out.println("Output: " + threeSum(nums1));
+
+        int[] nums2 = {0,1,1};
+        System.out.println("Output: " + threeSum(nums2));
+
+        int[] nums3 = {0,0,0};
+        System.out.println("Output: " + threeSum(nums3));
+    }
+}
